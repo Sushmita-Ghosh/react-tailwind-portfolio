dd1ee6ae033f0e9b69fa57a43f2917f8b810d2e4
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-12-18 22:36:43 -0800

    [Data Changes]

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,35 @@
-import about from "../assets/about.jpg";
+import chat from "../assets/projects/letschat.png";
+import bites from "../assets/projects/bigbites.jpg";
+import notes from "../assets/projects/notes.jpg";
 
 export const projects = [
   {
     title: "Big Bites",
-    imageSrc: about,
+    imageSrc: bites,
     description:
-      "This is a project made to learn the latest languages by building an app.",
-    skills: ["React", "Express", "Node"],
-    demo: "https://www.example.com",
-    source: "https://www.github.com",
+      "A Food Ordering Application developed using the public API provided by Swiggy",
+    skills: ["React", "Redux", "TailwindCSS"],
+    demo: "https://github.com/Sushmita-Ghosh/food_app",
+    source: "https://github.com/Sushmita-Ghosh/food_app",
     category: ["Frontend", "All"],
   },
   {
     title: "Google Keep Clone",
-    imageSrc: about,
+    imageSrc: notes,
     description:
-      "This is a project made to learn the latest languages by building an app.",
-    skills: ["React", "Express", "Node", "Sass"],
-    demo: "https://www.example.com",
-    source: "https://www.github.com",
+      "A react based notes taking app that implements all crud operations",
+    skills: ["React", "Firebase", "SCSS"],
+    demo: "https://github.com/Sushmita-Ghosh/google-keep-clone",
+    source: "https://github.com/Sushmita-Ghosh/google-keep-clone",
     category: ["Frontend", "All"],
   },
   {
     title: "Let's Chat",
-    imageSrc: about,
-    description:
-      "This is a project made to learn the latest languages by building an app.",
+    imageSrc: chat,
+    description: "A react based web application using MERN stack",
     skills: ["React", "Express", "Node"],
-    demo: "https://www.example.com",
-    source: "https://www.github.com",
+    demo: "https://github.com/Sushmita-Ghosh/mern_chat_app",
+    source: "https://github.com/Sushmita-Ghosh/mern_chat_app",
     category: ["FullStack", "All"],
   },
 ];
